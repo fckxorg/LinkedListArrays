e9ed80f7d8ab7b01a627f6b0e05b356a9eda3337
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2019-11-08 01:33:35 +0300

    deleteElement

--- main.cpp
+++ main.cpp
@@ -1,22 +1,44 @@
 #include <iostream>
 #include <stack>
 
-const int LIST_MAX_SIZE = 20;
 
 template<class T>
 class LinkedList {
  private:
-  size_t next[LIST_MAX_SIZE] = {};
-  size_t previous[LIST_MAX_SIZE] = {};
-  T data[LIST_MAX_SIZE] = {};
+  size_t* next;
+  size_t* previous;
+  T* data;
   std::stack<int> empty_cells;
   size_t n_elements;
+  size_t max_elements;
   size_t head;
   size_t tail;
  public:
-  LinkedList () : n_elements(0)
+  LinkedList (size_t max_elements) : n_elements(0)
   {
-    for(int i = LIST_MAX_SIZE - 1; i >=0; i--) empty_cells.push(i);
+    this->max_elements = max_elements;
+
+    for(int i = max_elements - 1; i >=0; i--) empty_cells.push(i);
+
+    data = new T[max_elements]();
+    next = new size_t[max_elements]();
+    previous = new size_t[max_elements]();
+  }
+
+  void deleteElement(size_t position)
+  {
+    if (position > max_elements)
+      {
+        perror ("Trying to delete element with position greater than max number of elements in list!");
+        return;
+      }
+    empty_cells.push(position);
+
+    data[position] = 0;
+    previous[next[position]] = previous[position];
+    next[previous[position]] = next[position];
+
+    n_elements--;
   }
 
   void pushHead (T value)
@@ -127,16 +149,12 @@ class LinkedList {
 
 int main ()
 {
-  LinkedList<int> my_list;
+  LinkedList<int> my_list = (10);
   my_list.pushTail (10);
-  my_list.pushTail (20);
   my_list.pushTail (30);
-  my_list.pushTail (40);
-  my_list.pushHead (50);
-  my_list.insertAfter (3, 100);
-  my_list.insertBefore (3, 200);
-
-  my_list.dump ("dump.dot");
+  my_list.pushTail (50);
+  my_list.deleteElement (1);
+  my_list.dump("dump.dot");
 
   return 0;
 }
